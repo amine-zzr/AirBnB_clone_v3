a6723d9abd9e237018499ce9582a8f7b1cf6dc95
amine-zzr <aminezoukri@gmail.com>
2024-05-26 22:41:01 +0000

    a handler for 404 errors that returns a JSON-formatted 404 status code response

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,12 @@ def teardown_appcontext(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """Handler for 404 errors that returns a JSON response."""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
