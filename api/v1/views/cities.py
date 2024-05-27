8ce5871f48a8eac3f8b36733cbb8ddebba66a1b5
amine-zzr <aminezoukri@gmail.com>
2024-05-27 22:21:53 +0000

    trying a new fix again

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -14,7 +14,7 @@ from models.city import City
                  strict_slashes=False)
 def get_cities(state_id):
     """Retrieves the list of all City objects of a State"""
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     cities = [city.to_dict() for city in state.cities]
@@ -24,7 +24,7 @@ def get_cities(state_id):
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """Retrieves a City object"""
-    city = storage.get("City", city_id)
+    city = storage.get(City, city_id)
     if city is None:
         abort(404)
     return jsonify(city.to_dict())
@@ -33,7 +33,7 @@ def get_city(city_id):
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """Deletes a City object"""
-    city = storage.get("City", city_id)
+    city = storage.get(City, city_id)
     if city is None:
         abort(404)
     storage.delete(city)
@@ -45,13 +45,13 @@ def delete_city(city_id):
                  strict_slashes=False)
 def create_city(state_id):
     """Creates a City"""
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     if not request.json:
-        abort(400, 'Not a JSON')
+        return abort(400, 'Not a JSON')
     if 'name' not in request.json:
-        abort(400, 'Missing name')
+        return abort(400, 'Missing name')
     data = request.get_json()
     new_city = City(**data)
     new_city.state_id = state_id
@@ -62,11 +62,11 @@ def create_city(state_id):
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     """Updates a City object"""
-    city = storage.get("City", city_id)
+    city = storage.get(City, city_id)
     if city is None:
         abort(404)
     if not request.json:
-        abort(400, 'Not a JSON')
+        return abort(400, 'Not a JSON')
     data = request.get_json()
     ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
     for key, value in data.items():
