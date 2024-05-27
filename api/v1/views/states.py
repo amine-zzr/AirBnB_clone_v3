002e04af7d1093a08250b8710e7d36479d0a64aa
amine-zzr <aminezoukri@gmail.com>
2024-05-27 23:12:49 +0000

    trying with the states API

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,10 +3,10 @@
 View for State objects that handles all default RESTful API actions
 """
 
-from flask import jsonify, request, abort
-from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
 from models import storage
 from models.state import State
+from api.v1.views import app_views
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -25,8 +25,8 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route(
-        '/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """Deletes a State object"""
     state = storage.get(State, state_id)
@@ -41,10 +41,11 @@ def delete_state(state_id):
 def create_state():
     """Creates a State"""
     if not request.json:
-        abort(400, 'Not a JSON')
-    if 'name' not in request.json:
-        abort(400, 'Missing name')
-    new_state = State(**request.get_json())
+        abort(400, description="Not a JSON")
+    data = request.get_json()
+    if 'name' not in data:
+        abort(400, description="Missing name")
+    new_state = State(**data)
     new_state.save()
     return jsonify(new_state.to_dict()), 201
 
@@ -56,7 +57,7 @@ def update_state(state_id):
     if state is None:
         abort(404)
     if not request.json:
-        abort(400, 'Not a JSON')
+        abort(400, description="Not a JSON")
     data = request.get_json()
     ignore_keys = ['id', 'created_at', 'updated_at']
     for key, value in data.items():
